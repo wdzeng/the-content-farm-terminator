b900e91816737a146e209eeed178db2c0bc42fc9
hyperbola <me@hyperbola.me>
2023-07-12 06:26:31 +0800

    feat: add comments for utility functions

--- src/scripts/util/array.ts
+++ src/scripts/util/array.ts
@@ -1,13 +1,18 @@
+// Removes duplicated elements in an array. The order of elements are preserved.
 export function removeDuplicates(a: string[]): string[] {
   const set = new Set<string>()
   return a.filter(e => !set.has(e) && set.add(e))
 }
 
+// Returns elements in array `a` but not in array `b` while preserves the order
+// in array `a`.
 export function subtractArray(a: string[], b: string[]): string[] {
   const set = new Set(b)
   return a.filter(x => !set.has(x))
 }
 
+// Returns elements appearing in both array `a` and `b` while the order is not
+// cared.
 export function intersectArray(a: string[], b: string[]): string[] {
   if (a.length < b.length) return intersectArray(b, a)
   // a.length >= b.length
