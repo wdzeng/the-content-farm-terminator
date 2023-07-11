65b4d9521de18e632d0d9224e02fdc8264341fc4
hyperbola <me@hyperbola.me>
2023-07-12 06:24:08 +0800

    fix: redo button in result search page not working

--- js/block.js
+++ js/block.js
@@ -7,7 +7,7 @@
 
     function addHints() {
         let $hint = $('<a class="TbwUpd fl" href="#" style="color: #808080;">' + chrome.i18n.getMessage('block') + '</a>').css('margin-left', '4px').click(function () {
-            
+
             const host = $('h3 a', $(this).parents('div.g'))[0].hostname;
 
             //Get block
@@ -21,10 +21,11 @@
 
             //Hide blocks
             $sec.fadeOut(800, function () {
-                let $redoText = $('<div class="g s"></div>')
+                const clz = host.replace('.', '-');
+                let $redoText = $('<div class="g s ' + clz + '"></div>')
                     .append($('<span>' + chrome.i18n.getMessage('redoText', host) + '</span>'));
                 let $redoBtn = $('<a href="#">' + chrome.i18n.getMessage('redo') + '</a>').click(function () {
-                    $redoText.hide(0);
+                    $('div.' + clz).hide(0);
                     $sec.fadeIn(800);
                     removeHost(host);
                     //Disable anchor action
