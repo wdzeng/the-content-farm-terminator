4cbafaf4b5961371d262c406436b6f09a854bf92
hyperbola <me@hyperbola.me>
2023-07-12 06:25:07 +0800

    refact: rename terminator.scss/js to inject.scss/js

--- src/scripts/content-script.ts
+++ src/scripts/content-script.ts
@@ -1,6 +1,6 @@
 // https://stackoverflow.com/a/53033388
 (async () => {
-  const src = chrome.runtime.getURL('/scripts/terminator.js')
+  const src = chrome.runtime.getURL('/scripts/inject.js')
   const contentMain = await import(src)
   contentMain.init()
 })()
